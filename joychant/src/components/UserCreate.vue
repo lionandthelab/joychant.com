e46caa67b0231f15c365a589f27f2f56b26cd36f
Hyungsuk.kim <Hyungsuk.kim@cmes.kr>
2021-10-18 22:59:41 +0900

    add announcement about end of funding

--- joychant/src/components/UserCreate.vue
+++ joychant/src/components/UserCreate.vue
@@ -1,40 +1,66 @@
+
 <template>
-  <div class="card card-body mt-4">
+<div>
+    <img v-bind:src="require('../assets/fund.png')" />
+  <div class="card card-body mt-4" id="forms">
+    
     <form @submit.prevent="onSubmit">
       <div class="form-group">
-        <label>Name</label>
+        <label>이름</label>
         <input v-model="form.name" class="form-control" required />
       </div>
 
       <div class="form-group mt-3">
-        <label>Email</label>
+        <label>연락처</label>
         <input
-          v-model="form.email"
+          v-model="form.phone"
           class="form-control"
-          type="email"
+          type="phone"
           required
         />
       </div>
 
+      <div class="form-group mt-4">
+        <label>문의 내용</label>
+        <input
+          v-model="form.question"
+          class="form-control"
+          type="question"
+          required
+        />
+      </div>
       <button type="submit" class="btn btn-success mt-3">
         Create User
       </button>
     </form>
   </div>
+  </div>
 </template>
-
 <script>
 import { createUser } from "@/firebase";
 import { reactive } from "vue";
 export default {
   setup() {
-    const form = reactive({ name: "", email: "" });
+    const form = reactive({ name: "", phone: "", question : "" });
     const onSubmit = async () => {
       await createUser({ ...form });
       form.name = "";
-      form.email = "";
+      form.phone = "";
+      form.question = "";
     };
     return { form, onSubmit };
   },
 };
 </script>
+<style scoped>
+  img{
+    width: 70%;
+    height: 70%;
+    align-self: center;
+  }
+  #forms{
+    width: 70%;
+    height: 70%;
+    align-self: center;
+  }
+</style>
--- joychant/src/views/Home.vue
+++ joychant/src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
+  <div>
+
+  
   <UserCreate />
-  <UserList />
+
+  </div>
 </template>
 
 <script>
 import UserCreate from '@/components/UserCreate.vue'
-import UserList from '@/components/UserList.vue'
 export default {
   name: 'Home',
-  components: { UserCreate, UserList }
+  components: { UserCreate }
 }
 </script>
